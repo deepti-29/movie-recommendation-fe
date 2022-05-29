e41bd436edfca0c5bb56894a5688d14ab86a59d9
Deepti <s.deepti2906@gmail.com>
2022-05-29 17:53:07 +0530

    added frontend code

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <LandingPage />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import LandingPage from "@/components/LandingPage.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    LandingPage,
+  },
+})
+export default class Home extends Vue {}
+</script>
